a44842fb306315bf9d4158b4bc21766be6e36cbb
SamuelACrumpler <samuel.a.crumpler@gmail.com>
2020-07-28 13:46:17 -0700

    Updates

---
+++ precache-manifest.57bd9a0b4070e20ab6facf411a1d7cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb5c282166aabbca04cbd8dbf70f90f",
+    "url": "/capstone/index.html"
+  },
+  {
+    "revision": "d05663afc93b81dd9d22",
+    "url": "/capstone/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aac934b58a46b3066274",
+    "url": "/capstone/static/css/main.10559651.chunk.css"
+  },
+  {
+    "revision": "d05663afc93b81dd9d22",
+    "url": "/capstone/static/js/2.d5b59255.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/capstone/static/js/2.d5b59255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac934b58a46b3066274",
+    "url": "/capstone/static/js/main.88921005.chunk.js"
+  },
+  {
+    "revision": "161021f6cc4fdfe169bf",
+    "url": "/capstone/static/js/runtime-main.8d42e8f7.js"
+  }
+]);
--- src/js/logging.js
+++
@@ -1,6 +0,0 @@
-export function checkSession(){
-    if (localStorage.getItem("isLoggedIn") === 'false' || !localStorage.getItem("isLoggedIn")) {
-        this.props.history.push('/')
-    }
-}
-
