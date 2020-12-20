d186adec0aca6ff675edf818d1731f9f004a9e1a
SamuelACrumpler <samuel.a.crumpler@gmail.com>
2020-12-20 09:59:57 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 'extended': 'false' }));
 
-app.use("/user", user);
+app.use("/usertest", user);
 app.use("/customer", customer);
 app.use("/appointment", appointment);
 
